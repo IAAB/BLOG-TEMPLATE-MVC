09c450c00c8f0426fc600832666884a9c34340d5
Ivo Angelov <iaa_b@yahoo.com>
2016-03-27 19:11:46 +0300

    Some fails deleted

--- BlogTemplateMvc/Global.asax.cs
+++ BlogTemplateMvc/Global.asax.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using BlogTemplateMvc.Context;
+using BlogTemplateMvc.Migrations;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +15,10 @@ namespace BlogTemplateMvc
     {
         protected void Application_Start()
         {
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new RazorViewEngine());
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<BlogMVCDbContext, Configuration>());
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
